82a7595f8b3bcb9cf54ad4f72d7118246985e399
j-kyoda <j.kyoda@gmail.com>
2017-09-19 23:29:11 +0900

    Change install version to emacs25.3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # install.emacs
 emacs installer script for CentOS7.
 
-This script install emacs 25.1.
+This script install emacs 25.3.
 
--- install_emacs.sh
+++ install_emacs.sh
@@ -6,9 +6,9 @@ yum -y install gcc make ncurses-devel
 
 # install emacs
 cd /usr/local/src
-curl -O http://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.gz
-tar zxvfp emacs-25.1.tar.gz
-cd emacs-25.1
+curl -O http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz
+tar zxvfp emacs-25.3.tar.gz
+cd emacs-25.3
 ./configure -without-x -without-selinux
 make
 make install
