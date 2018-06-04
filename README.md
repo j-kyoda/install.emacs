d1d0c0cc490d313f9bb0394594c9784841f77b5d
j-kyoda <j.kyoda@gmail.com>
2018-06-04 23:56:38 +0900

    Change install version to emacs-26.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # install.emacs
 emacs installer script for CentOS7.
 
-This script install emacs-nox 25.3.
+This script install emacs-nox 26.1.
 
 
 How to install
--- install_emacs.sh
+++ install_emacs.sh
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # required packages(CentOS7)
-yum -y install gcc make ncurses-devel
+yum -y install gcc make ncurses-devel autoconf gnutls-devel
 
 # install emacs
 cd /usr/local/src
-curl -O http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz
-tar zxvfp emacs-25.3.tar.gz
-cd emacs-25.3
+curl -O http://ftp.gnu.org/gnu/emacs/emacs-26.1.tar.gz
+tar zxvfp emacs-26.1.tar.gz
+cd emacs-26.1
+bash autogen.sh
 ./configure -without-x -without-selinux
 make
 make install
