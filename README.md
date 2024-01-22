526c192100ece3b2c7b81fbd0430d2151ec08e64
j-kyoda <j.kyoda@gmail.com>
2024-01-22 14:04:26 +0900

    Change install version to emacs-29.2

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # install.emacs
-emacs installer script for Debian 11.
+emacs installer script for Debian 12.
 
-This script install emacs-nox 29.1.
+This script install emacs-nox 29.2.
 
 
 How to install
--- install_emacs.sh
+++ install_emacs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-# required packages(debian11)
+# required packages(debian12)
 apt-get install -y gcc make libncurses-dev autoconf libneon27-gnutls-dev
 
 # install emacs
 cd /usr/local/src
-curl -O http://ftp.gnu.org/gnu/emacs/emacs-29.1.tar.gz
-tar zxvfp emacs-29.1.tar.gz
-cd emacs-29.1
+curl -O http://ftp.gnu.org/gnu/emacs/emacs-29.2.tar.gz
+tar zxvfp emacs-29.2.tar.gz
+cd emacs-29.2
 bash autogen.sh
 ./configure -without-x -without-selinux
 make
